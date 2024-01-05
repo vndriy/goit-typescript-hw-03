babfa211f037ffa4b68f418e2bd288fbfa1839b1
vndriy <vndriy@users.noreply.github.com>
2024-01-05 17:20:12 +0100

    OOP Typescript Homework

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,48 @@
+class Key {
+    private signature: number = Math.random();
+    
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean = false;
+  protected tenants: Person[] = [];
+
+  constructor(protected key: Key) {}
+
+  abstract openDoor(key: Key): void;
+
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log("Welcome! Door open!");
+    } else {
+      console.log("Door closed");
+    }
+  }
+}
+
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log("Welcome! Door open!");
+    } else {
+      console.log("Door closed!");
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -7,5 +52,4 @@ house.openDoor(person.getKey());
 
 house.comeIn(person);
 
-
-export {};
+export { };
